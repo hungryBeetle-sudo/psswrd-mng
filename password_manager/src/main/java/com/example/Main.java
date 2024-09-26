fadaadb2033125e5ee5c35f255091778d56436c7
hungryBeetle-sudo <erik.ts@outlook.com>
2024-09-26 19:23:08 -0300

    Create basic menu.

--- password_manager/src/main/java/com/example/Main.java
+++ password_manager/src/main/java/com/example/Main.java
@@ -1,6 +1,32 @@
 package com.example;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        boolean finish = false;
+        ArrayList<Credential> credentials = new ArrayList<Credential>();
+        Scanner scanner = new Scanner(System.in);
+
+        while(!finish){
+
+            System.out.println("""
+            OPTIONS:
+            1 - Get credential
+            2 - Insert credential
+            3 - Update credential
+            4 - Delete credential
+            5 - Exit
+            """);
+            
+            int option = scanner.nextInt();
+
+            switch(option){
+                case 1, 2, 3, 4, 5 -> finish = true;
+                default -> System.out.println("Insert a valid option!");
+            }
+        }
+
+        scanner.close();
     }
 }
